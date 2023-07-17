447f474ed06c6cab6842617a1f2ce7cd062a2b04
ksehat <kan.sehat@gmail.com>
2023-07-17 08:35:43 +0330

    Flight724 scraper is added to the code.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, Response
 import json
 from sepehr_scraper import sepehr_scrapper
+from flight724_scrapper import Flight724Scrapper
 # from flask import Flask
 from waitress import serve
 
@@ -21,5 +22,13 @@ def home():
             )
 
 
+@app1.route('/flight724', methods=['POST'])
+def flight724_api():
+    if (request.method == 'POST'):
+        data = json.loads(request.json)
+        f_scrapper = Flight724Scrapper(data['orig'], data['dest'], data['num'])
+        f_scrapper.get_flight724_route()
+
+
 if __name__ == '__main__':
-    serve(app1, host='192.168.40.155', port='3000')
+    serve(app1, host='192.168.115.17', port='3000')
