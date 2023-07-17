4f93fd0feb7faa0cccdd4ab58d9d172bc550e71d
ksehat <kan.sehat@gmail.com>
2023-07-17 08:36:41 +0330

    The code is refactored to be compatible to the multiprocessing.

--- flight724_scrapper.py
+++ flight724_scrapper.py
@@ -53,6 +53,7 @@ class Flight724Scrapper:
         self.orig = orig
         self.dest = dest
         self.days_num = days_num
+        self.day_num = 0
 
     def get_flight724_route(self):
         try:
@@ -73,108 +74,137 @@ class Flight724Scrapper:
                                 '//a[contains(@class, "weekday") and not(contains(@class, "invalid"))]').click()
             driver.find_element(By.XPATH, '//*[@id="search_submit"]').click()
 
-            price_list = []
-            type_list = []
-            airline_list = []
-            datetime_list = []
-            cap_list = []
-            extra_info = []
-            class_list = []
-            for n1 in range(1, len(driver.find_elements(By.XPATH, '//div[@class="resu "]')) + 1):
-                elem1 = driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]')
-                ActionChains(driver).move_to_element(elem1).perform()
+            for day_num in range(self.day_num, self.days_num):
+                if day_num > 0:
+                    WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located((By.XPATH, f'//*[@id="search_result_from"]/div[2]/a[2]')))
+                    driver.find_element(By.XPATH, '//*[@id="search_result_from"]/div[2]/a[2]').click()
                 try:
-                    price_list.append(
-                        driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="price"]/span').text)
+                    WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, f'//div[@class="resu "]')))
                 except:
-                    price_list.append(None)
-                try:
-                    type_list.append(
-                        driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]/span').text)
-                except:
-                    type_list.append(None)
-                try:
-                    datetime_list.append(
-                        driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="date"]').text)
-                except:
-                    datetime_list.append(None)
+                    if driver.find_element(By.XPATH, '//*[@id="fromprice"]/div[2]/div[1]').text == '0 تا ( نتیجه)':
+                        continue
+                    else:
+                        raise 1
 
-                try:
-                    cap_list.append(
-                        driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="user"]').text)
-                except:
-                    cap_list.append(None)
+                price_list = []
+                type_list = []
+                airline_list = []
+                datetime_list = []
+                cap_list = []
+                extra_info = []
+                class_list = []
+                flight_date = driver.find_element(By.XPATH, '//*[@id="r_city_date"]/span').text
+                for n1 in range(1, len(driver.find_elements(By.XPATH, '//div[@class="resu "]')) + 1):
+                    elem1 = driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]')
+                    ActionChains(driver).move_to_element(elem1).perform()
+                    try:
+                        price_list.append(
+                            driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="price"]/span').text)
+                    except:
+                        price_list.append(None)
+                    try:
+                        type_list.append(
+                            driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]/span').text)
+                    except:
+                        type_list.append(None)
+                    try:
+                        datetime_list.append(
+                            driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="date"]').text)
+                    except:
+                        datetime_list.append(None)
 
-                try:
-                    elem11 = driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')
+                    try:
+                        cap_list.append(
+                            driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="user"]').text)
+                    except:
+                        cap_list.append(None)
+
+                    try:
+                        elem11 = driver.find_element(By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')
+                        ActionChains(driver).move_to_element(elem11).perform()
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located(
+                                (By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
+                        airline_list.append(driver.find_element(By.XPATH,
+                                                                f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[2]/strong[2]').text)
+                    except:
+                        airline_list.append(None)
+                    try:
+                        # ActionChains(driver).move_to_element(elem11).perform()
+                        # WebDriverWait(driver, 10).until(
+                        #     EC.presence_of_element_located((By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
+                        extra_info.append(driver.find_element(By.XPATH,
+                                                              f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[3]').text.split(
+                            '\n'))
+                    except:
+                        extra_info.append(None)
+
+                    try:
+                        # ActionChains(driver).move_to_element(elem11).perform()
+                        # WebDriverWait(driver, 10).until(
+                        #     EC.presence_of_element_located((By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
+                        class_list.append(driver.find_element(By.XPATH,
+                                                              f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[4]').text.split(
+                            '\n')[0].split(":")[1])
+                    except:
+                        class_list.append(None)
+                    elem11 = driver.find_element(By.XPATH, f'//div[@class="resu "]')
                     ActionChains(driver).move_to_element(elem11).perform()
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
-                    airline_list.append(driver.find_element(By.XPATH,
-                                                            f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[2]/strong[2]').text)
-                except:
-                    airline_list.append(None)
-                try:
-                    # ActionChains(driver).move_to_element(elem11).perform()
-                    # WebDriverWait(driver, 10).until(
-                    #     EC.presence_of_element_located((By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
-                    extra_info.append(driver.find_element(By.XPATH,
-                                                          f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[3]').text.split(
-                        '\n'))
-                except:
-                    extra_info.append(None)
 
-                try:
-                    # ActionChains(driver).move_to_element(elem11).perform()
-                    # WebDriverWait(driver, 10).until(
-                    #     EC.presence_of_element_located((By.XPATH, f'//div[@class="resu "][{n1}]/div[@class="code"]')))
-                    class_list.append(driver.find_element(By.XPATH,
-                                                          f'//div[@class="resu "][{n1}]/div[4]/div[1]/div[2]/div[4]').text.split(
-                        '\n')[0].split(":")[1])
-                except:
-                    class_list.append(None)
-                elem11 = driver.find_element(By.XPATH, f'//div[@class="resu "]')
-                ActionChains(driver).move_to_element(elem11).perform()
+                df = pd.DataFrame({
+                    'flight_date': [flight_date] * len(extra_info),
+                    'price_list': price_list,
+                    'type_list': type_list,
+                    'datetime_list': datetime_list,
+                    'cap_list': cap_list,
+                    'airline_list': airline_list,
+                    'class_list': class_list,
+                    'extra_info': extra_info,
+                    'scrap_date': [str(datetime.datetime.now())] * len(extra_info)
+                })
 
-            df = pd.DataFrame({
-                'price_list': price_list,
-                'type_list': type_list,
-                'datetime_list': datetime_list,
-                'cap_list': cap_list,
-                'airline_list': airline_list,
-                'class_list':class_list,
-                'extra_info': extra_info
-            })
+                result_dict = df.to_dict('records')
 
-            result_dict = df.to_dict('records')
+                # Connect to the MongoDB server
+                MONGODB_HOST = '192.168.115.17'
+                MONGODB_PORT = 27017
+                MONGODB_USER = 'kanan'
+                MONGODB_PASS = '123456'
+                MONGODB_DB = 'fids_DB'
+                client = MongoClient(MONGODB_HOST, MONGODB_PORT,
+                                     username=MONGODB_USER,
+                                     password=MONGODB_PASS,
+                                     authSource=MONGODB_DB)
 
-            # Connect to the MongoDB server
-            MONGODB_HOST = '192.168.115.17'
-            MONGODB_PORT = 27017
-            MONGODB_USER = 'kanan'
-            MONGODB_PASS = '123456'
-            MONGODB_DB = 'fids_DB'
-            client = MongoClient(MONGODB_HOST, MONGODB_PORT,
-                                 username=MONGODB_USER,
-                                 password=MONGODB_PASS,
-                                 authSource=MONGODB_DB)
+                # Get the database and collection
+                db = client['flight724_DB']
+                collection = db['flight724']
 
-            # Get the database and collection
-            db = client['flight724_DB']
-            collection = db['flight724']
-
-            # Insert a document
-            if result_dict:
-                collection.insert_many(result_dict)
-
+                # Insert a document
+                if result_dict:
+                    try:
+                        # This is necessary to prevent race condition while multiple threads are writing into database
+                        session = client.start_session()
+                        session.start_transaction()
+                        collection.insert_many(result_dict, session=session)
+                        session.commit_transaction()
+                    except:
+                        session.abort_transaction()
+                        self.day_num = day_num
+                        try:
+                            driver.close()
+                        except:
+                            pass
+                        print(f'race in database writing is occured.')
+                        self.get_flight724_route()
         except Exception as e:
+            self.day_num = day_num
             try:
                 driver.close()
             except:
                 pass
             print(f'There occured an error in the sepehr_scraper.py. {e}')
             self.get_flight724_route()
-
-
-f_scrapper = Flight724Scrapper('MHD', 'THR', 2)
-f_scrapper.get_flight724_route()
