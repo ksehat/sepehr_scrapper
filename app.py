5cd178236d30b818ae55a79635a4efca0b6d9d86
ksehat <kan.sehat@gmail.com>
2023-07-31 08:44:51 +0330

    Input changed to a list.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, request, jsonify, Response
 import json
 from sepehr_scraper import sepehr_scrapper
@@ -26,9 +28,14 @@ def home():
 def flight724_api():
     if (request.method == 'POST'):
         data = json.loads(request.json)
-        f_scrapper = Flight724Scrapper(data['orig'], data['dest'], data['num'])
-        f_scrapper.get_flight724_route()
+        f_scrapper = Flight724Scrapper(data[0], data[1], data[2])
+        try:
+            print(f'Scrapping started for the {data[0], data[1], data[2]} parameters at {datetime.datetime.now()}.')
+            f_scrapper.get_flight724_route()
+            print(f'Scrapping finished for the {data[0], data[1], data[2]} parameters at {datetime.datetime.now()}.')
+        except:
+            print('Error occured in the f_scrapper.get_flight724_route().')
 
 
 if __name__ == '__main__':
-    serve(app1, host='192.168.115.17', port='3000')
+    serve(app1, host='192.168.115.17', port='3000', threads = 30)
