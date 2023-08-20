664cf35d64c8e2ce6e8ae74b715d436823d24786
ksehat <kan.sehat@gmail.com>
2023-08-20 10:17:43 +0330

    trampoline is added to clean the cache.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import datetime
-
 from flask import Flask, request, jsonify, Response
 import json
 from sepehr_scraper import sepehr_scrapper
-from flight724_scrapper import Flight724Scrapper
+from flight724_scrapper import Flight724Scrapper, trampoline
 # from flask import Flask
 from waitress import serve
 
@@ -29,13 +28,19 @@ def flight724_api():
     if (request.method == 'POST'):
         data = json.loads(request.json)
         f_scrapper = Flight724Scrapper(data[0], data[1], data[2])
-        try:
-            print(f'Scrapping started for the {data[0], data[1], data[2]} parameters at {datetime.datetime.now()}.')
-            f_scrapper.get_flight724_route()
-            print(f'Scrapping finished for the {data[0], data[1], data[2]} parameters at {datetime.datetime.now()}.')
-        except:
-            print('Error occured in the f_scrapper.get_flight724_route().')
+        result = trampoline(f_scrapper.get_flight724_route)
+        if result:
+            return Response(
+                "Success.",
+                status=200,
+            )
+        else:
+            return Response(
+                "Error occured.",
+                status=400,
+            )
 
 
 if __name__ == '__main__':
-    serve(app1, host='192.168.115.17', port='3000', threads = 30)
+    print('app.py started.')
+    serve(app1, host='192.168.115.17', port='3000', threads=30)
