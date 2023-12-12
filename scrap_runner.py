440ecfbfbc82bd6d4d428a27374843e2ed64e4d8
k.sehat <kan.sehat@gmail.com>
2023-12-12 09:24:43 +0330

    nothing important.

--- scrap_runner.py
+++ scrap_runner.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+import numpy as np
 import requests
 import json
 import pandas as pd
@@ -5,41 +8,47 @@ import concurrent.futures
 import threading
 
 
-def process_df(df, num_workers):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+def process_df(process_dict):
+
         while True:
-            list(executor.map(runner, df.itertuples(index=False)))
+            with concurrent.futures.ThreadPoolExecutor(max_workers=process_dict['num_of_workers']) as executor:
+                list(executor.map(runner, process_dict['df'].itertuples(index=False)))
+            print(f'the {process_dict["df"]} is done and im going to sleep {process_dict["interval"]}')
+            time.sleep(process_dict['interval'] * 3600)
 
 
 def runner(data):
-    print(data)
-    r = requests.post(url='http://192.168.115.17:3000/flight724', json=json.dumps(list(data)))
+    try:
+        r = requests.post(url='http://192.168.115.17:3000/scrap_runner', json=json.dumps(list(data)))
+        print(list(data), datetime.datetime.now(), r)
+    except:
+        runner(data)
+
+def num_of_workers_for_group(interval):
+    # if interval <= 2:
+    #     return 5
+    # if  interval > 2 and interval<:
+    #     return 5
+    # else:
+    return 5
 
 
-scrap_data = pd.read_excel('C:/Users\Administrator\Desktop\Projects/Flight724_Sc.xlsx')
-dur_cols = [col for col in scrap_data.columns if col.startswith('dur')]
+scrap_data = pd.read_csv('E:\Projects\sepehr_scrapper\source for scrap/ROUT.csv')
+# TODO: after implementation of alibaba and flytoday scrapers this line should be deleted
+scrap_data = scrap_data[scrap_data['site']=='alameer.ir']
 
-# Create a dictionary to store the new dataframes
-dfs = {}
+scrap_data['per_hour'] = 24/scrap_data['perday']
 
-# Iterate over the 'dur' columns
-for col in dur_cols:
-    # Create a new dataframe with the first two columns and the current 'dur' column
-    dfs[f'df_{col}'] = scrap_data[['orig', 'dest', col]]
+data_dict = {}
+for interval in scrap_data['per_hour'].unique():
+    data_dict[interval] = {}
+    data_dict[interval]['df'] = scrap_data[scrap_data['per_hour'] == interval]
+    data_dict[interval]['num_of_workers'] = num_of_workers_for_group(interval)
+    data_dict[interval]['interval'] = interval
 
-# Create a list of arguments for each thread
-
-thread_args = [
-    (dfs['df_dur4'], 7),
-    (dfs['df_dur3'], 3),
-    (dfs['df_dur2'], 1),
-    (dfs['df_dur1'], 1)
-]
-
-# Create and start the threads using a for loop
 threads = []
-for args in thread_args:
-    t = threading.Thread(target=process_df, args=args)
+for k in data_dict.keys():
+    t = threading.Thread(target=process_df, args=[data_dict[k]])
     t.start()
     threads.append(t)
 
