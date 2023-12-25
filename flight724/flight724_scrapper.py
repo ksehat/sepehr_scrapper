c7a3eebf0b939640af79f1a54302ff90a5200fc7
k.sehat <kan.sehat@gmail.com>
2023-12-25 16:01:25 +0330

    last updated verssion of scrapper.

--- flight724/flight724_scrapper.py
+++ flight724/flight724_scrapper.py
@@ -10,26 +10,25 @@ from selenium.webdriver.chrome.service import Service
 
 
 class Flight724Scrapper:
-    def __init__(self, orig, dest, days_num):
+    def __init__(self, orig, dest, days_num=1, flight_date=None):
         self.orig = orig
         self.dest = dest
         self.days_num = days_num
-        self.day_num_text = None
         self.day_num = 0
-        self.error_exit = 0
+        self.flight_date = flight_date
         self.orig_dest_are_extracted = False
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--incognito')
+        self.url = "http://flight724.com/"
 
     def get_flight724_route(self):
         try:
-            url: str = ("http://flight724.com/")
-            options = webdriver.ChromeOptions()
-            options.add_argument('--ignore-certificate-errors')
-            options.add_argument('--incognito')
-            self.driver = webdriver.Chrome(service=Service(
-                "E:\Projects\sepehr_scrapper\chromedriver.exe"), options=options)
             try:
+                self.driver = webdriver.Chrome(service=Service(
+                    "E:\Projects\sepehr_scrapper\chromedriver.exe"), options=self.options)
                 if not self.orig_dest_are_extracted:
-                    self.driver.get(url=url)
+                    self.driver.get(url=self.url)
                     self.driver.find_element(By.XPATH, '//*[@id="search_auto_from"]').send_keys(self.orig + '\n')
                     self.orig_for_url = \
                         self.driver.find_element(By.XPATH, '//*[@id="search_auto_from"]').get_attribute('value').split(
@@ -47,9 +46,13 @@ class Flight724Scrapper:
                 self.get_flight724_route()
 
             for self.day_num in range(self.day_num, self.days_num):
-                flight_date = str(datetime.datetime.now() + datetime.timedelta(days=self.day_num)).split(' ')[0]
-                self.driver.get(
-                    url=f'http://flight724.com/Ticket-{self.orig_for_url}-{self.dest_for_url}.html?t={flight_date}')
+                if self.flight_date:
+                    self.driver.get(
+                        url=f'http://flight724.com/Ticket-{self.orig_for_url}-{self.dest_for_url}.html?t={self.flight_date}')
+                else:
+                    flight_date = str(datetime.datetime.now() + datetime.timedelta(days=self.day_num)).split(' ')[0]
+                    self.driver.get(
+                        url=f'http://flight724.com/Ticket-{self.orig_for_url}-{self.dest_for_url}.html?t={flight_date}')
 
                 orig_list = []
                 dest_list = []
@@ -125,37 +128,41 @@ class Flight724Scrapper:
                     elem11 = self.driver.find_element(By.XPATH, f'//div[@class="resu "]')
                     ActionChains(self.driver).move_to_element(elem11).perform()
 
+                extra_info_dict = [{item.split(': ')[0]: item.split(': ')[1] for item in sub_list} for sub_list in extra_info]
+
                 df = pd.DataFrame({
+                    'source': ['flight724'] * len(price_list),
                     'orig': orig_list,
                     'dest': dest_list,
                     'dur': days_num_list,
                     'ticket_class': selling_type_list,
-                    'flight_date': [flight_date] * len(extra_info),
+                    'flight_date': [flight_date] * len(price_list),
                     'price': price_list,
                     'flight_num': flight_num_list,
                     'dep_time': dep_time_list,
                     'capacity': cap_list,
                     'airline': airline_list,
                     'flight_class': flight_class_list,
+                    'total_cap': [sub_dict['ظرفیت '] for sub_dict in extra_info_dict],
+                    'airplane_model': [sub_dict['نوع هواپیما'] for sub_dict in extra_info_dict],
                     'extra_info': extra_info,
                     'scrap_date': [str(datetime.datetime.now())] * len(extra_info)
                 })
 
                 result_dict = df.to_dict('records')
-
-                MONGODB_HOST = '192.168.115.17'
-                MONGODB_PORT = 24048
-                MONGODB_USER = 'kanan'
-                MONGODB_PASS = '123456'
-                MONGODB_DB = 'flight724_DB'
-                client = MongoClient(MONGODB_HOST, MONGODB_PORT,
-                                     username=MONGODB_USER,
-                                     password=MONGODB_PASS,
-                                     authSource=MONGODB_DB)
-
-                db = client['flight724_DB']
-                collection = db['flight724']
                 if result_dict:
+                    MONGODB_HOST = '192.168.115.17'
+                    MONGODB_PORT = 24048
+                    MONGODB_USER = 'kanan'
+                    MONGODB_PASS = '123456'
+                    MONGODB_DB = 'flight724_DB'
+                    client = MongoClient(MONGODB_HOST, MONGODB_PORT,
+                                         username=MONGODB_USER,
+                                         password=MONGODB_PASS,
+                                         authSource=MONGODB_DB)
+
+                    db = client['flight724_DB']
+                    collection = db['flight724']
                     collection.insert_many(result_dict)
                 self.day_num += 1
             self.driver.close()
@@ -165,9 +172,10 @@ class Flight724Scrapper:
                 self.driver.close()
             except:
                 pass
-            self.error_exit = 1
             return False
 
 
-f_scrapper = Flight724Scrapper('THR', 'MHD', 3)
-f_scrapper.get_flight724_route()
+f_scrapper = Flight724Scrapper('THR', 'MHD', 10)
+result = False
+while result == False:
+    result = f_scrapper.get_flight724_route()
