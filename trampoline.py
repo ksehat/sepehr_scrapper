8761a5cfdc218be0662938a1891341d99db1e8cf
k.sehat <kan.sehat@gmail.com>
2023-12-31 09:09:51 +0330

    trampoline and all scrappers are changed in order to handle an empty dataframe and return message.

--- trampoline.py
+++ trampoline.py
@@ -2,7 +2,10 @@ def trampoline(func, *args, **kwargs):
     while True:
         result = func(*args, **kwargs)
         try:
-            if not result.empty:
-                return result
+            if result[0]:
+                if result[1].empty:
+                    return 'There are no flights at this date.'
+                else:
+                    return result[1]
         except:
             pass
