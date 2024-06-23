c5c5eb47157c3cebb87fcb56d2e431f32984756b
ksehat <k.sehat@gmail.com>
2024-06-23 04:24:45 -0700

    [Debug & Modify]: error handling in the persian to gregorian date is implemented. When there is any error in the date, it is returned as intact.

--- utils/persian_to_gregorian_date.py
+++ utils/persian_to_gregorian_date.py
@@ -10,33 +10,27 @@ def normalize_persian_text(text):
 
 
 def persian_to_datetime(persian_date_str):
-    # if ':' in persian_date_str:
-    #     year, month, day = map(int, persian_date_str.split(' ')[0].split("/"))
-    # else:
-    #     year, month, day = map(int, persian_date_str.split("/"))
-    #
-    # persian_date = JalaliDateTime(year, month, day)
-    # christian_date = persian_date.to_gregorian()
-    #
-    # return christian_date.date()
-    parts = persian_date_str.split('/')
-
-    if len(parts) == 3:
-        # Check if it's in the format 'DD/MM/YYYY'
-        if parts[1].isdigit():
-            year, month, day = map(int, parts)
+    try:
+        parts = persian_date_str.split('/')
+    
+        if len(parts) == 3:
+            # Check if it's in the format 'DD/MM/YYYY'
+            if parts[1].isdigit():
+                year, month, day = map(int, parts)
+            else:
+                # Handle format 'DD/month_name/YYYY' (using persiantools)
+                year = int(parts[2])
+                month = int(persiantools.jdatetime.MONTH_NAMES_FA.index(normalize_persian_text(parts[1])))
+                day = int(parts[0])
         else:
-            # Handle format 'DD/month_name/YYYY' (using persiantools)
-            year = int(parts[2])
-            month = int(persiantools.jdatetime.MONTH_NAMES_FA.index(normalize_persian_text(parts[1])))
-            day = int(parts[0])
-    else:
-        raise ValueError("Invalid Persian date format. Please use 'DD/MM/YYYY' or 'DD/month_name/YYYY'.")
+            return persian_date_str
 
-    persian_date = JalaliDateTime(year, month, day)
-    christian_date = persian_date.to_gregorian()
+        persian_date = JalaliDateTime(year, month, day)
+        christian_date = persian_date.to_gregorian()
 
-    return christian_date.date()
+        return christian_date.date()
+    except:
+        return persian_date_str
 
 
 def persian_time_to_datetime(persian_time_str):
